c0e024aa35ac571df003685f4b96adbacddbabff
Sara Ghali <45029076+SaraGhali@users.noreply.github.com>
2022-06-13 17:39:17 +0200

    Project page

---
+++ VTS-Management/Models/ProjectsCustom.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace VTS_Management.Models
+{
+    [MetadataType(typeof(ProjectMetadata))]
+    public partial class Project
+    {
+    }
+    public class ProjectMetadata
+    {
+        [Display(Name ="ID")]
+        public int ProjectId { get; set; }
+
+        [Display(Name = "Name")]
+        [Required]
+        public string ProjectName { get; set; }
+
+        [Display(Name = "Type")]
+        [Required]
+        public string ProjectType { get; set; }
+
+        [Display(Name = "Employee")]
+        [Required]
+        public string ProjectEmployee { get; set; }
+
+        [Display(Name = "Cost")]
+        [Required]
+        public Nullable<double> ProjectCost { get; set; }
+
+        [Display(Name = "Profit")]
+        [Required]
+        public Nullable<double> ProjectProfit { get; set; }
+
+        [Display(Name = "Customer")]
+        [Required]
+        public string ProjectCustomer { get; set; }
+
+        [Display(Name = "Sales")]
+         public Nullable<decimal> ProjectSales { get; set; }
+    }
+}
